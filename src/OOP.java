f5f5e56bd6e07bfb7fa08ac22e7778418ece8f5c
ikauematos <kaue178hotimail@gmail.com>
2023-01-19 07:06:51 -0300

    começando projeto

---
+++ src/OOP.java
@@ -0,0 +1,24 @@
+// Criando um Novo Banco | O que tem dentro de uma conta-bancaria
+//Agencia,Numero da conta,deposito,saldo,Sacar,Fechar conta,Transferir para outra conta,Titular
+
+
+public class OOP {
+    class Conta{
+        double saldo = this.saldo;
+        int agencia = this.agencia;
+        int numero = this.numero;
+
+        double deposito = this.deposito;
+        String titular = this.titular;
+
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println();
+
+    }
+
+
+}
