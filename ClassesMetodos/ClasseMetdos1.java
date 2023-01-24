a4ccbb9690c2311ee0309a8959c49ed748548dfe
ikauematos <kaue178hotimail@gmail.com>
2023-01-24 06:55:55 -0300

    Atualização

---
+++ ClassesMetodos/ClasseMetdos1.java
@@ -0,0 +1,40 @@
+package ClassesMetodos;
+
+public class ClasseMetdos1 {
+
+
+    //corpo classe;
+
+    // Dentro de um corpo de uma classes existe atributos e comportamentos;
+
+    //NEMBROS
+
+    //Por exemplo atributos são tudo aquilo que conseguimos atribuir como caracteristicas por exemplo um carro
+    // Ele possui uma caracteristica COR,MODELO,PLACA,RODA,POTENCIA isso e considerado como uma caracteristica direta do carro
+
+    //Substantivos de atributos --- COMPORTAMENTOS,dando exemplo de um (carro) o carro possui coportamentos por
+    // exemplo,FREIAR,ACELERAR,BUZINAR,ABASTECER;
+
+
+    //COMPORTAMENTOS SÃO METÓDOS POR EXEMPLO FUNÇÕES |ALGORITMOS
+
+    //ATRIBUTOS SÃO DADOS POR EXEMPLO: INT,STRING,BOOLEAN,DATE
+
+    //Objetos e uma instancia
+
+
+
+
+
+
+    //O que podemos definir dentro de uma CLASSE?
+
+    //Podemos definir Variaveis e constantes
+    //Construtor
+    //Métodos
+    //classes
+    }
+
+
+
+
